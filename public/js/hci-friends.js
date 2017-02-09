a78c97e93c7db55e04ad1587252fe157ad6f7310
Juntao Zhu <juz088@ucsd.edu>
2017-02-09 11:45:29 -0800

    lab5

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".name").click(Click);
 }
 
 function anagrammedName(name) {
@@ -43,4 +44,12 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
+}
+
+function Click(e) {
+	e.preventDefault();
+	var name = $(this).text();
+	var nName = anagrammedName(name);
+	console.log(nName);
+	$(this).text(nName);
 }
--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,12 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	var newFriend = {};
+	newFriend.name = req.query.name;
+	newFriend.description = req.query.description;
+	newFriend.imageURL = "http://lorempixel.com/400/400/people";
+	data.friends.push(newFriend);
+	console.log("new friend added");	
+	res.render('index', data);
+
  }
